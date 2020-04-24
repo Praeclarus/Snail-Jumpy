366867b16ebe90b0c0a35d2f7492a3f04c5f1b10
praeclarus <epicrelyt@gmail.com>
2020-04-23 23:07:57 -0700

    Fixed bug when resizing window and there is now an incredibly basic profiler

---
+++ src/snail_jumpy_debug.h
@@ -0,0 +1,63 @@
+#if !defined(SNAIL_JUMPY_DEBUG_H)
+#define SNAIL_JUMPY_DEBUG_H
+
+struct profiled_block {
+    char *Name;
+    u64 CycleCount;
+    u32 Level;
+};
+
+struct profile_data {
+    profiled_block Blocks[256];
+    u32 CurrentBlockIndex;
+    u32 CurrentLevel;
+};
+
+// TODO(Tyler): Move this once there is a more appropriate spot for it
+global profile_data GlobalProfileData;
+
+internal u32
+BeginProfiledBlock(char *Name){
+    GlobalProfileData.Blocks[GlobalProfileData.CurrentBlockIndex].Name = Name;
+    GlobalProfileData.Blocks[GlobalProfileData.CurrentBlockIndex].Level = GlobalProfileData.CurrentLevel;
+    GlobalProfileData.CurrentLevel++;
+    
+    u32 Result = GlobalProfileData.CurrentBlockIndex++;
+    return(Result);
+}
+
+internal void
+EndProfiledBlock(u32 Index, u64 CycleCount){
+    GlobalProfileData.Blocks[Index].CycleCount = CycleCount;
+    
+    Assert(GlobalProfileData.CurrentLevel > 0);
+    GlobalProfileData.CurrentLevel--;
+}
+
+struct timed_scope {
+    u64 StartCycleCount;
+    u32 Index;
+    
+    timed_scope(char *BlockName){
+        Index = BeginProfiledBlock(BlockName);
+        this->StartCycleCount = __rdtsc();
+    }
+    
+    ~timed_scope(){
+        EndProfiledBlock(Index, __rdtsc()-StartCycleCount);
+    }
+};
+
+#define TIMED_SCOPE(Id, Name) timed_scope TimedScope##Id(Name);
+#define TIMED_FUNCTION() TIMED_SCOPE(FUNC, __FUNCTION__)
+
+#define GetCycles(Id) \
+SafeRatio0(GlobalProfileData.TotalCycleCounts[ProfilerIndex##Id], GlobalProfileData.ProfileCounts[ProfilerIndex##Id])
+
+#define GetTimedScopeCycles(Id) \
+SafeRatio0(GlobalProfileData.TotalCycleCounts[TimedScope##Id.Index], GlobalProfileData.ProfileCounts[TimedScope##Id.
+
+#define BEGIN_BLOCK(Id) u32 ProfileIndex##Id = BeginProfiledBlock(#Id); u64 StartCycle##Id = __rdtsc();
+#define END_BLOCK(Id) EndProfiledBlock(ProfileIndex##Id, __rdtsc()-StartCycle##Id);
+
+#endif //SNAIL_JUMPY_DEBUG_H
---
+++ src/snail_jumpy_debug_ui.cpp
@@ -0,0 +1,19 @@
+internal void
+RenderAllProfileData(temporary_memory *RenderMemory, render_group *RenderGroup,
+                     f32 X, f32 *Y, f32 XAdvance, f32 YAdvance){
+    for(u32 ProfileIndex = 0;
+        ProfileIndex < ArrayCount(GlobalProfileData.Blocks);
+        ProfileIndex++){
+        profiled_block *Block = &GlobalProfileData.Blocks[ProfileIndex];
+        if(Block->Name == 0){
+            continue;
+        }
+        
+        f32 ActualX = X + (XAdvance*(f32)Block->Level);
+        RenderFormatString(RenderMemory, RenderGroup, &GlobalFont,
+                           {0.0f, 0.0f, 0.0f, 1.0f},
+                           ActualX, *Y, 0.0f,
+                           "%s: %'llucy", Block->Name, Block->CycleCount);
+        *Y -= YAdvance;
+    }
+}
